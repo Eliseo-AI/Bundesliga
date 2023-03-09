d1f5ebd26904a3bd65b793f86ace2ebed322472b
Eliseo <73314455+Eli-2020@users.noreply.github.com>
2023-03-09 16:59:45 -0500

    Update Pj-Bundesliga.py

--- Pj-Bundesliga.py
+++ Pj-Bundesliga.py
@@ -23,8 +23,7 @@ with st.beta_expander("Explanation & Tips"):
      3. Average Goals for.
      4. Graph consolidate average of Wins, Losses and Draws. 
      5. Graph consolidate average of Wins, Losses and Draws.
-     6. Choose view between normalized over 100k and non-normalized data. 
-     ''')
+     6. Choose view between normalized over 100k and non-normalized data.""")
 total_clubs = len(df_bundes)
 Average_Points = df_bundes["Points"].mean()
 Average_Goals = df_bundes["GF"].mean()
